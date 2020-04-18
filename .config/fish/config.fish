6095db66e7e43421c9f0a957fb38fee01498b242
Vincent Cogne <banananana@protonmail.com>
2020-04-18 14:51:24 +0200

    Fix autostart x

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ export EDITOR=vim
 
 # Start X at login
 if status is-login
-  if test -z "$DISPLAY" -a $XDG_VTNR = 1
+  if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
     exec startx -- -keeptty
   end
 end
