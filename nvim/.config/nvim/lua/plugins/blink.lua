775a4728f1280d63ce8d5b97e7cd2516b39d79ba
Vincent Cogne <banananana@protonmail.com>
2026-03-30 16:40:30 +0200

    Update neovim config

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -6,16 +6,8 @@ end
 blink.setup({
   keymap = {
     preset = 'default',
-    ['<Tab>'] = {
-      function(cmp)
-        if cmp.is_visible() then
-          return cmp.select_next()
-        end
-        return cmp.show_and_insert()
-      end,
-      'fallback',
-    },
-    ['<S-Tab>'] = { 'select_prev', 'fallback' },
+    ['<Tab>'] = { 'insert_next', 'fallback' },
+    ['<S-Tab>'] = { 'insert_prev', 'fallback' },
     ['<CR>'] = { 'accept', 'fallback' },
     ['<C-Space>'] = { 'show', 'show_documentation', 'hide_documentation' },
   },
@@ -27,7 +19,7 @@ blink.setup({
     list = {
       selection = {
         preselect = false,
-        auto_insert = false,
+        auto_insert = true,
       },
     },
   },
@@ -38,23 +30,15 @@ blink.setup({
     enabled = true,
     keymap = {
       preset = 'cmdline',
-      ['<Tab>'] = {
-        function(cmp)
-          if cmp.is_visible() then
-            return cmp.select_next()
-          end
-          return cmp.show_and_insert()
-        end,
-        'fallback',
-      },
-      ['<S-Tab>'] = { 'select_prev', 'fallback' },
+      ['<Tab>'] = { 'insert_next', 'fallback' },
+      ['<S-Tab>'] = { 'insert_prev', 'fallback' },
       ['<CR>'] = { 'accept_and_enter', 'fallback' },
     },
     completion = {
       list = {
         selection = {
           preselect = false,
-          auto_insert = false,
+          auto_insert = true,
         },
       },
     },
